f233c04341e3e1fe5b3716ca712c9b45cb7f5e85
Mike Caputo <mike@caputo.io>
2019-04-28 19:45:56 -0500

    Add basic treasure (magic item) generator

---
+++ treasure.js
@@ -0,0 +1,594 @@
+const supportedSources = ['xanathar', 'dmg'];
+
+const items = [
+  {
+    name: "Armor of Gleaming",
+    rarity: "Common",
+    type: "Armor",
+    source: "xanathar"
+  },
+    {
+        name: "Bead of Nourishment",
+        rarity: "Common",
+        type: "Wondrous item",
+        source: "xanathar"
+    },
+    {
+        name: "Bead of Refreshment",
+        rarity: "Common",
+        type: "Wondrous item",
+        source: "xanathar"
+    },
+    {
+        name: "Boots of false tracks",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },
+    {
+        name: "Candle of the Deep",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },
+    {
+        name: "Cast-off Armor",
+        rarity: "Common",
+        type: "Armor",
+        source: "xanathar"
+    },
+    {
+        name: "Charlatan's Die",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },
+    {
+        name: "Cloak of billowing",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },
+    {
+        name: "Cloak of Many Fashions",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },
+    {
+        name: "Clockwork Amulet",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },
+    {
+        name: "Clothes of Mending",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },
+    {
+        name: "Dark Shard Amulet",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },
+    {
+        name: "Dread helm",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },
+    {
+        name: "Ear horn of hearing",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },
+    {
+        name: "Enduring spellbook",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },
+    {
+        name: "Ersatz eye",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },
+    {
+        name: "Hat of Vermin",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },
+    {
+        name: "Hat of Wizardry",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },
+    {
+        name: "Heward's Handy Spice Pouch",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },
+    {
+        name: "Horn of Silent Alarm",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },
+    {
+        name: "Instrument of Illusions",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },
+    {
+        name: "Instrument of Scribing",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },
+    {
+        name: "Lock of Trickery",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },
+    {
+        name: "Moon-touched Sword",
+        rarity: "Common",
+        type: "Weapon",
+        source: "xanathar"
+    },
+    {
+        name: "Mystery Key",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },
+    {
+        name: "Orb of Direction",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },    {
+        name: "Orb of Time",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },
+    {
+        name: "Perfume of Bewitching",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },
+    {
+        name: "Pipe of Smoke Monsters",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },
+    {
+        name: "Pole of Angling",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },
+    {
+        name: "Pole of Collapsing",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },
+    {
+        name: "Pot of Awakening",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },
+    {
+        name: "Potion of Climbing",
+        rarity: "Common",
+        type: "Potion",
+        source: "dmg"
+    },
+    {
+        name: "Potion of Healing",
+        rarity: "Common",
+        type: "Potion",
+        source: "dmg"
+    },
+    {
+        name: "Rope of Mending",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },
+    {
+        name: "Ruby of the War Mage",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },    {
+        name: "Shield of Expression",
+        rarity: "Common",
+        type: "Armor",
+        source: "xanathar"
+    },    {
+        name: "Smoldering Armor",
+        rarity: "Common",
+        type: "Armor",
+        source: "xanathar"
+    },
+    {
+        name: "Spell scroll (cantrip)",
+        rarity: "Common",
+        type: "Scroll",
+        source: "dmg"
+    },
+    {
+        name: "Spell Scroll (1st level)",
+        rarity: "Common",
+        type: "Scroll",
+        source: "dmg"
+    },
+    {
+        name: "Staff of Adornment",
+        rarity: "Common",
+        type: "Staff",
+        source: "xanathar"
+    },
+    {
+        name: "Staff of Birdcalls",
+        rarity: "Common",
+        type: "Staff",
+        source: "xanathar"
+    },
+    {
+        name: "Staff of Flowers",
+        rarity: "Common",
+        type: "Staff",
+        source: "xanathar"
+    },
+    {
+        name: "Talking Doll",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },
+    {
+        name: "Tankard of Sobriety",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },
+    {
+        name: "Unbreakable arrow",
+        rarity: "Common",
+        type: "Weapon",
+        source: "xanathar"
+    },
+    {
+        name: "Veteran's Cane",
+        rarity: "Common",
+        type: "Wondrous Item",
+        source: "xanathar"
+    },
+    {
+        name: "Walloping Ammunition",
+        rarity: "Common",
+        type: "Weapon",
+        source: "xanathar"
+    },
+    {
+        name: "Wand of Conducting",
+        rarity: "Common",
+        type: "Wand",
+        source: "xanathar"
+    },
+    {
+        name: "Wand of Pyrotechnics",
+        rarity: "Common",
+        type: "Wand",
+        source: "xanathar"
+    },
+    {
+        name: "Wand of Scowls",
+        rarity: "Common",
+        type: "Wand",
+        source: "xanathar"
+    },
+    {
+        name: "Wand of Smiles",
+        rarity: "Common",
+        type: "Wand",
+        source: "xanathar"
+    },
+    {
+        name: "Alchemy jug",
+        rarity: "Uncommon",
+        type: "Wondrous Item",
+        source: "dmg"
+    },
+    {
+        name: "Ammunition, +1",
+        rarity: "Uncommon",
+        type: "Weapon",
+        source: "dmg"
+    },
+    {
+        name: "Bag of Holding",
+        rarity: "Uncommon",
+        type: "Wondrous Item",
+        source: "dmg"
+    },
+    {
+        name: "Cap of Waterbreathing",
+        rarity: "Uncommon",
+        type: "Wondrous Item",
+        source: "dmg"
+    },
+    {
+        name: "Cloak of the Manta Ray",
+        rarity: "Uncommon",
+        type: "Wondrous Item",
+        source: "dmg"
+    },
+    {
+        name: "Decanter of Endless Water",
+        rarity: "Uncommon",
+        type: "Wondrous Item",
+        source: "dmg"
+    },
+    {
+        name: "Driftglobe",
+        rarity: "Uncommon",
+        type: "Wondrous Item",
+        source: "dmg"
+    },
+    {
+        name: "Dust of Disappearance",
+        rarity: "Uncommon",
+        type: "Wondrous Item",
+        source: "dmg"
+    },
+    {
+        name: "Dust of Dryness",
+        rarity: "Uncommon",
+        type: "Wondrous Item",
+        source: "dmg"
+    },
+    {
+        name: "Dust of Sneezing and Choking",
+        rarity: "Uncommon",
+        type: "Wondrous Item",
+        source: "dmg"
+    },
+    {
+        name: "Elemental Gem",
+        rarity: "Uncommon",
+        type: "Wondrous Item",
+        source: "dmg"
+    },
+    {
+        name: "Eyes of Minute Seeing",
+        rarity: "Uncommon",
+        type: "Wondrous Item",
+        source: "dmg"
+    },
+    {
+        name: "Goggles of Night",
+        rarity: "Uncommon",
+        type: "Wondrous Item",
+        source: "dmg"
+    },
+    {
+        name: "Helm of Comprehending Languages",
+        rarity: "Uncommon",
+        type: "Wondrous Item",
+        source: "dmg"
+    },
+    {
+        name: "Immovable Rod",
+        rarity: "Uncommon",
+        type: "Rod",
+        source: "dmg"
+    },
+    {
+        name: "Keoghtmo's Ointment",
+        rarity: "Uncommon",
+        type: "Wondrous Item",
+        source: "dmg"
+    },
+    {
+        name: "Lantern of Revealing",
+        rarity: "Uncommon",
+        type: "Wondrous Item",
+        source: "dmg"
+    },
+    {
+        name: "Mariner's Armor",
+        rarity: "Uncommon",
+        type: "Armor",
+        source: "dmg"
+    },
+    {
+        name: "Oil of Slipperiness",
+        rarity: "Uncommon",
+        type: "Potion",
+        source: "dmg"
+    },
+    {
+        name: "Periapt of Health",
+        rarity: "Uncommon",
+        type: "Wondrous Item",
+        source: "dmg"
+    },
+    {
+        name: "Philter of Love",
+        rarity: "Uncommon",
+        type: "Potion",
+        source: "dmg"
+    },
+    {
+        name: "Potion of Animal Friendship",
+        rarity: "Uncommon",
+        type: "Potion",
+        source: "dmg"
+    },
+    {
+        name: "Potion of Fire Breath",
+        rarity: "Uncommon",
+        type: "Potion",
+        source: "dmg"
+    },
+    {
+        name: "Potion of Greater Healing",
+        rarity: "Uncommon",
+        type: "Potion",
+        source: "dmg"
+    },
+    {
+        name: "Potion of Growth",
+        rarity: "Uncommon",
+        type: "Potion",
+        source: "dmg"
+    },
+    {
+        name: "Potion of Hill Giant Strength",
+        rarity: "Uncommon",
+        type: "Potion",
+        source: "dmg"
+    },
+    {
+        name: "Potion of Poison",
+        rarity: "Uncommon",
+        type: "Potion",
+        source: "dmg"
+    },
+    {
+        name: "Potion of Resistance",
+        rarity: "Uncommon",
+        type: "Potion",
+        source: "dmg"
+    },
+    {
+        name: "Potion of Resistance",
+        rarity: "Uncommon",
+        type: "Potion",
+        source: "dmg"
+    },
+    {
+        name: "Potion of Water Breathing",
+        rarity: "Uncommon",
+        type: "Potion",
+        source: "dmg"
+    },
+    {
+        name: "Ring of Swimming",
+        rarity: "Uncommon",
+        type: "Ring",
+        source: "dmg"
+    },
+    {
+        name: "Robe of Useful Items",
+        rarity: "Uncommon",
+        type: "Wondrous Item",
+        source: "dmg"
+    },
+    {
+        name: "Rope of Climbing",
+        rarity: "Uncommon",
+        type: "Wondrous Item",
+        source: "dmg"
+    },
+    {
+        name: "Saddle of the Cavalier",
+        rarity: "Uncommon",
+        type: "Wondrous Item",
+        source: "dmg"
+    },
+    {
+        name: "Sending Stones",
+        rarity: "Uncommon",
+        type: "Wondrous Item",
+        source: "dmg"
+    },
+    {
+        name: "Spell Scroll (2nd level)",
+        rarity: "Uncommon",
+        type: "Scroll",
+        source: "dmg"
+    },
+
+    {
+        name: "Spell Scroll (3rd level)",
+        rarity: "Uncommon",
+        type: "Scroll",
+        source: "dmg"
+    },
+    {
+        name: "Wand of Magic Detection",
+        rarity: "Uncommon",
+        type: "Wand",
+        source: "dmg"
+    },
+    {
+        name: "Wand of Secrets",
+        rarity: "Uncommon",
+        type: "Wand",
+        source: "dmg"
+    }
+];
+
+
+const getItem = function(options) {
+    options = options || {};
+    let item = null;
+    if (options.type) {
+        let filteredItems = items.filter(it => it.type.toLowerCase() === options.type.toLowerCase());
+        item = filteredItems[Math.floor(Math.random() * filteredItems.length)]
+    }
+    else if (options.rarity) {
+        let filteredItems = items.filter(it => it.rarity.toLowerCase() === options.rarity.toLowerCase());
+        item = filteredItems[Math.floor(Math.random() * filteredItems.length)]
+    }
+    else if (options.source) {
+        let filteredItems = items.filter(it => it.source.toLowerCase() === options.source.toLowerCase());
+        item = filteredItems[Math.floor(Math.random() * filteredItems.length)]
+    }
+    else {
+        item = items[Math.floor(Math.random() * items.length)];
+    }
+
+    console.log(`Name: ${item.name}`);
+    console.log(`Rarity: ${item.rarity}`);
+    console.log(`Type: ${item.type}`);
+}
+
+let options = {}
+process.argv.forEach(function (val, index, array) {
+    switch(val) {
+        case '-t':
+        case '--type':
+            options.type = array[index + 1];
+            break;
+        case '-r':
+        case '--rarity':
+            options.rarity = array[index + 1];
+            break;
+        case '-s':
+        case '--source':
+            options.source = array[index + 1];
+            break;
+    }
+
+});
+console.log(options)
+getItem(options);
